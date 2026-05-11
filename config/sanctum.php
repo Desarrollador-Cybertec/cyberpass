77dd264e777516a6d269d3dee6d30e670bfce547
m4nt3k1ll4 <juanchin23ad@gmail.com>
2026-05-11 18:06:03 -0500

    fix: Update Sanctum token expiration default value from 5 to 10 minutes

--- config/sanctum.php
+++ config/sanctum.php
@@ -50,7 +50,7 @@ return [
     |
     */
 
-    'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 5),
+    'expiration' => env('SANCTUM_TOKEN_EXPIRATION', 10),
 
     /*
     |--------------------------------------------------------------------------
